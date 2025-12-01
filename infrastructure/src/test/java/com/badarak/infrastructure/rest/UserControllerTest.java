6a50ede2e37278066a8bcfd3a8ee157b105c157e
Aliou <badarak20@gmail.com>
2025-12-01 12:04:15 +0100

    make UserController depends on the IN port

--- infrastructure/src/test/java/com/badarak/infrastructure/rest/UserControllerTest.java
+++ infrastructure/src/test/java/com/badarak/infrastructure/rest/UserControllerTest.java
@@ -1,7 +1,8 @@
 package com.badarak.infrastructure.rest;
 
+import com.badarak.domain.exception.ResourceNotFoundException;
 import com.badarak.domain.model.User;
-import com.badarak.domain.port.out.UserRepositoryPort;
+import com.badarak.domain.port.in.UserServicePort;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -17,10 +18,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 import java.util.UUID;
 
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static java.util.UUID.fromString;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.data.domain.PageRequest.of;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -32,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(UserController.class)
 class UserControllerTest {
     @MockitoBean
-    private UserRepositoryPort repository;
+    private UserServicePort userService;
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -42,7 +41,7 @@ class UserControllerTest {
     void should_return_error_when_user_not_found() throws Exception {
         //Given
         UUID uuid = UUID.randomUUID();
-        when(repository.findById(uuid)).thenReturn(empty());
+        when(userService.findById(uuid)).thenThrow(ResourceNotFoundException.class);
 
         mockMvc.perform(get("/api/users/{id}", uuid))
                 .andExpect(status().isNotFound());
@@ -52,7 +51,7 @@ class UserControllerTest {
     void should_return_user() throws Exception {
         // Given
         UUID uuid = UUID.randomUUID();
-        when(repository.findById(uuid)).thenReturn(of(new User(uuid, "test", "test@gmail.com")));
+        when(userService.findById(uuid)).thenReturn(new User(uuid, "test", "test@gmail.com"));
 
         // When && Then
         mockMvc.perform(get("/api/users/{id}", uuid))
@@ -65,7 +64,7 @@ class UserControllerTest {
     void should_save_user() throws Exception {
         //Given
         CreateOrUpdateUserRequest request = new CreateOrUpdateUserRequest("test", "test@gmail.com");
-        when(repository.save(any(User.class))).thenReturn(new User(UUID.randomUUID(), "test", "test@gmail.com"));
+        when(userService.createUser(anyString(), anyString())).thenReturn(new User(UUID.randomUUID(), "test", "test@gmail.com"));
 
         //When & Then
         mockMvc.perform(post("/api/users")
@@ -88,8 +87,8 @@ class UserControllerTest {
     void should_update_user() throws Exception {
         //Given
         UUID id = fromString("8a8bd2cc-caa6-4fb3-9ad4-2244393cb608");
-        when(repository.findById(id)).thenReturn(of(new User(id, "name", "test@gmail.com")));
-        when(repository.save(any(User.class))).thenReturn(new User(id, "new_name", "new_test@gmail.com"));
+        when(userService.findById(id)).thenReturn(new User(id, "name", "test@gmail.com"));
+        when(userService.updateUser(id, "new_name", "new_test@gmail.com")).thenReturn(new User(id, "new_name", "new_test@gmail.com"));
 
         CreateOrUpdateUserRequest request = new CreateOrUpdateUserRequest(
                 "new_name",
@@ -112,7 +111,7 @@ class UserControllerTest {
                 of(0, 10),
                 1);
 
-        when(repository.findAll(Mockito.any(Pageable.class))).thenReturn(users);
+        when(userService.listUsers(Mockito.any(Pageable.class))).thenReturn(users);
         //When & Then
         mockMvc.perform(get("/api/users"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
