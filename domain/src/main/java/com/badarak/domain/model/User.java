31acea9f7bace9e4a77986913460a33bc8f64545
Aliou <badarak20@gmail.com>
2025-12-03 13:55:59 +0100

    use lombok pour auto generate getters and constructor

--- domain/src/main/java/com/badarak/domain/model/User.java
+++ domain/src/main/java/com/badarak/domain/model/User.java
@@ -1,33 +1,17 @@
 package com.badarak.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.Objects;
 import java.util.UUID;
 
+@Getter
+@AllArgsConstructor
 public class User {
-    private UUID id;
-    private String name;
-    private String email;
-
-    public User(UUID id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public User() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+    private final UUID id;
+    private final String name;
+    private final String email;
 
     @Override
     public boolean equals(Object o) {
